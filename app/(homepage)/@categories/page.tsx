cd1309208f65e5f5cb38b7867f45c43b514e9640
Joeniesus <132261842+jnssvdl@users.noreply.github.com>
2024-11-04 20:01:38 +0800

    feat: styles home

--- app/(homepage)/@categories/page.tsx
+++ app/(homepage)/@categories/page.tsx
@@ -4,60 +4,86 @@ import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { useRouter } from "next/navigation";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 type Category = {
-    id: number;
-    name: string;
-    picture_url: string;
+  id: number;
+  name: string;
+  picture_url: string;
 };
 
 const CategoriesPage = () => {
-    const [categories, setCategories] = useState<Category[]>([]);
-    const router = useRouter();
+  const [categories, setCategories] = useState<Category[]>([]);
+  const router = useRouter();
 
-    useEffect(() => {
-        const getCategories = async () => {
-            const supabase = createClientComponentClient();
-            const { data, error } = await supabase.from("categories").select();
-            setCategories(data);
-        };
-        getCategories();
-    }, []);
-
-    const redirectCategory = (category: Category) => {
-        const queryParams = new URLSearchParams(window.location.search);
-        queryParams.set("category", category.name);
-        router.push(`/search?${queryParams.toString()}`);
+  useEffect(() => {
+    const getCategories = async () => {
+      const supabase = createClientComponentClient();
+      const { data, error } = await supabase.from("categories").select();
+      setCategories(data);
     };
+    getCategories();
+  }, []);
 
-    return (
-        <div>
-            <h1>Categories</h1>
-            <div>
-                {categories.map((category) => {
-                    return (
-                        <button
-                            key={category.id}
-                            onClick={() => {
-                                redirectCategory(category);
-                            }}
-                        >
-                            <h2>{category.name}</h2>
-                            <div>
-                                <Image
-                                    src={category.picture_url}
-                                    alt={category.name}
-                                    width={50}
-                                    height={50}
-                                    priority
-                                />
-                            </div>
-                        </button>
-                    );
-                })}
-            </div>
-        </div>
-    );
+  const redirectCategory = (category: Category) => {
+    const queryParams = new URLSearchParams(window.location.search);
+    queryParams.set("category", category.name);
+    router.push(`/search?${queryParams.toString()}`);
+  };
+
+  return (
+    <section className="flex h-64 flex-col items-center justify-center gap-4 border-b border-zinc-200 bg-zinc-50">
+      <h1 className="text-2xl font-bold text-zinc-500">Categories</h1>
+      <Carousel
+        opts={{
+          align: "start",
+        }}
+        className="w-4/5"
+      >
+        <CarouselContent>
+          {categories.map((category) => {
+            return (
+              <CarouselItem
+                key={category.id}
+                className="md:basis-1/2 lg:basis-1/4"
+              >
+                <div className="p-1">
+                  <Card>
+                    <CardContent className="flex aspect-auto items-center justify-center p-6">
+                      <div
+                        role="button"
+                        onClick={() => redirectCategory(category)}
+                      >
+                        <div className="h-16 w-16">
+                          <Image
+                            src={category.picture_url}
+                            alt={category.name}
+                            width={40}
+                            height={40}
+                            priority
+                          />
+                        </div>
+                        <h2>{category.name}</h2>
+                      </div>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            );
+          })}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </section>
+  );
 };
 
 export default CategoriesPage;
--- app/(homepage)/@shops/page.tsx
+++ app/(homepage)/@shops/page.tsx
@@ -5,49 +5,74 @@ import Image from "next/image";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import Link from "next/link";
 import placeholder from "@/assets/placeholder.webp";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 type Shop = {
-    id: number;
-    acronym: string;
-    logo_url: string;
+  id: number;
+  acronym: string;
+  logo_url: string;
 };
 
 const Shops = () => {
-    const [shops, setShops] = useState<Shop[]>([]);
+  const [shops, setShops] = useState<Shop[]>([]);
 
-    useEffect(() => {
-        const getShops = async () => {
-            const supabase = createClientComponentClient();
-            const { data, error } = await supabase
-                .from("shops")
-                .select("id, acronym, logo_url");
-            setShops(data);
-        };
-        getShops();
-    }, []);
+  useEffect(() => {
+    const getShops = async () => {
+      const supabase = createClientComponentClient();
+      const { data, error } = await supabase
+        .from("shops")
+        .select("id, acronym, logo_url");
+      setShops(data);
+    };
+    getShops();
+  }, []);
 
-    return (
-        <div>
-            <div>Shops</div>
-            <div>
-                {shops &&
-                    shops.map((shop) => {
-                        return (
-                            <Link key={shop.id} href={`/shop/${shop.acronym}`}>
-                                <h2>{shop.acronym}</h2>
-                                <Image
-                                    width={50}
-                                    height={50}
-                                    src={shop.logo_url}
-                                    alt={`${shop.acronym} logo`}
-                                    priority
-                                />
-                            </Link>
-                        );
-                    })}
-            </div>
-        </div>
-    );
+  return (
+    <section className="flex h-64 flex-col items-center justify-center gap-4 border-b border-zinc-200 bg-zinc-50">
+      <h1 className="text-2xl font-bold text-zinc-500">Shops</h1>
+      <Carousel
+        opts={{
+          align: "start",
+        }}
+        className="w-4/5"
+      >
+        <CarouselContent>
+          {shops.map((shop) => {
+            return (
+              <CarouselItem key={shop.id} className="md:basis-1/2 lg:basis-1/4">
+                <div className="p-1">
+                  <Card>
+                    <CardContent className="flex aspect-auto items-center justify-center p-6">
+                      <Link key={shop.id} href={`/shop/${shop.acronym}`}>
+                        <Image
+                          width={50}
+                          height={50}
+                          src={shop.logo_url}
+                          alt={`${shop.acronym} logo`}
+                          priority
+                          className="rounded-full"
+                        />
+                        <p>{shop.acronym}</p>
+                      </Link>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            );
+          })}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </section>
+  );
 };
 
 export default Shops;
